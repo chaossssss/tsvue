9e7f5cd38a502e2af28961cc95ab0debb158792c
LAPTOP-UAONIT64\admin <1033729276@qq.com>
2021-02-08 14:39:42 +0800

    fix:测试点东西

---
+++ src/views/Calc.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <div>+</div>
+    <!-- <div>{{ times }}</div>
+    <div>{{ text }}</div> -->
+    <div>{{ ValA }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class Calc extends Vue {
+  ValA: string = "hello world";
+  ValB: number = 222;
+  // data(): { times: number; text: string } {
+  //   return {
+  //     times: 2,
+  //     text: '2222'
+  //   }
+  // }
+  @Prop() private msg!: string
+}
+</script>
+<style scoped lang="less">
+.hello {
+  color: red;
+}
+</style>
